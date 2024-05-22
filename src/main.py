3c55f81c9f067d8b8623221e9902432eb7bf3c92
rasmussaha <147138735+rasmussaha@users.noreply.github.com>
2024-05-22 11:36:20 +0000

    Update, currency converter

--- src/main.py
+++ src/main.py
@@ -1,22 +1,42 @@
-# Main code
 import requests
-import json
 
-# URL for the Tomorrow.io Weather Forecast API
-url = "https://api.tomorrow.io/v4/weather/forecast?location=orebro&timesteps=1d&units=metric&apikey=nukb3RL1iHdl4Pp4o0j58ONKsbRoVMxr"
+# API key and url
+api_key = "bVpcjeNUDqsgsTxvAbkmA2QAFsJiHk"
+url = "https://www.amdoren.com/api/currency.php"
 
-headers = {"accept": "application/json"}
+while True:
+    # Get user input
+    first_conversion = input("What currency do you want to convert from? (e.g., EUR, USD): ").upper()
+    second_conversion = input("What currency do you want to convert to? (e.g., EUR, USD): ").upper()
+    amount = input("What amount do you want to convert? ")
 
-# Send the GET request
-response = requests.get(url, headers=headers)
+    # The URL query parameters
+    params = {
+        "api_key": api_key,
+        "from": first_conversion,
+        "to": second_conversion,
+        "amount": amount
+    }
 
-# Check if the request was successful
-if response.status_code == 200:
+ # Make the API request
+    response = requests.get(url, params=params)
+    response.raise_for_status()
     data = response.json()
-    # Print each key-value pair in the response
-    for day in data.get('timelines', {}).get('daily', []):
-        for key, value in day.items():
-            print(f"{key}: {value}")
-        print("\n---\n")  # Separate each day's data
-else:
-    print(f"HTTP Error: {response.status_code}")
+
+    data['error'] == 0:
+    converted_amount = data['amount']
+    print(f"{amount} {first_conversion} is equal to {converted_amount} {second_conversion}.")
+
+    # Ask the user if they want to continue converting
+    while True:
+        stop = input("Do you want to continue converting? yes/no: ").strip().lower()
+        if stop == "no":
+            print("Stopping the conversion process.")
+            break
+        elif stop == "yes":
+            break
+        else:
+            print("Invalid input. Please enter 'yes' or 'no'.")
+
+    if stop == "no":
+        break
